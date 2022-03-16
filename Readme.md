a36ca12487525ac06f56c8b2a2e6de487a4cd075
sriyadhazwar <96353147+sriyadhazwar@users.noreply.github.com>
2022-03-16 14:33:19 +0700

    Add files via upload

---
+++ Readme.md
@@ -0,0 +1,3 @@
+npm install 
+npm install redux
+npm run dev
---
+++ next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    images: {
+        domains: ['i.ibb.co','ibb.co', 'fakestoreapi.com']
+    },
+    env: {
+        stripe_public_key: process.env.STRIPE_PUBLIC_KEY
+    }
+}
